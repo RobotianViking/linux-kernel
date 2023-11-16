66573451ad263820d172cf6ddb15706c2376348f
Niels Prosch <niels.h.prosch@gmail.com>
2023-11-16 18:50:14 +0100

    Setting loglevel to dbg and rename module

--- spi-protocol-sample/spi-protocol-sample.c
+++ spi-protocol-sample/spi-protocol-sample.c
@@ -10,16 +10,8 @@
 #include <linux/crc32.h>
 #include <linux/timekeeping.h>
 
-#define SPI_MODULE      "spi_protocol_sample"
+#define SPI_MODULE      "spi-protocol-device"
 #define RX_BUFFER_SIZE  10*1024*4
-// #define DEBUG_DUMP_SPI // DUMP SPI device on succesful probe
-// #define DEBUG_DUMP_CRC // DUMP data and CRC32 alternatives on each received
-                          // packet from SPI slave. Used for getting the right 
-                          // CRC32 from STM32F4 - which is challenging.
-
-/*
- *    DTS fragments: see spi-protocol-sample.dts
- */
 
 /**
  * @brief Structure for holding device state across driver callbacks
@@ -31,6 +23,13 @@ typedef struct _drvdata_t {
         u8 *rx_data;                     /* Data buffer for receiving */
 } drvdata_t;
 
+
+#define DEV_DEBUG       dev_info
+// #define DEBUG_DUMP_SPI // DUMP SPI device on succesful probe
+// #define DEBUG_DUMP_CRC // DUMP data and CRC32 alternatives on each received
+                          // packet from SPI slave. Used for getting the right 
+                          // CRC32 from STM32F4 - which is challenging.
+
 /**
  * Forward declarations
 */
@@ -38,8 +37,8 @@ static int get_block_sync(struct spi_device *spidev, size_t n, u8 *buf);
 static irq_handler_t top_ready_handler = NULL; /* Use default top half */
 static irqreturn_t bottom_ready_handler(int irq, void *dev_id);
 static int get_block_sync(struct spi_device *spidev, size_t n, u8 *buf);
-static int spi_probe(struct spi_device *spidev);
-static int spi_remove(struct spi_device *spidev);
+static int module_probe(struct spi_device *spidev);
+static int module_remove(struct spi_device *spidev);
 static int __init spi_module_init(void);
 static void __exit spi_module_exit(void);
 
@@ -48,15 +47,15 @@ static void __exit spi_module_exit(void);
 */
 #ifdef DEBUG_DUMP_SPI
 static void dump_spi_device(struct spi_device *spidev) {
-        dev_info(&spidev->dev, "SPI device dump\n");
-        dev_info(&spidev->dev, "===============\n");
-        dev_info(&spidev->dev, "max_speed_hz: %d\n", spidev->max_speed_hz);
-        dev_info(&spidev->dev, "chip_select: %d\n", spidev->chip_select);
-        dev_info(&spidev->dev, "bits_per_word: %d\n", spidev->bits_per_word);
-        dev_info(&spidev->dev, "rt: %d\n", spidev->rt);
-        dev_info(&spidev->dev, "mode: 0x%x\n", spidev->mode);
-        dev_info(&spidev->dev, "irq: %d\n", spidev->irq);
-        dev_info(&spidev->dev, "===============\n");
+        DEV_DEBUG(&spidev->dev, "SPI device dump\n");
+        DEV_DEBUG(&spidev->dev, "===============\n");
+        DEV_DEBUG(&spidev->dev, "max_speed_hz: %d\n", spidev->max_speed_hz);
+        DEV_DEBUG(&spidev->dev, "chip_select: %d\n", spidev->chip_select);
+        DEV_DEBUG(&spidev->dev, "bits_per_word: %d\n", spidev->bits_per_word);
+        DEV_DEBUG(&spidev->dev, "rt: %d\n", spidev->rt);
+        DEV_DEBUG(&spidev->dev, "mode: 0x%x\n", spidev->mode);
+        DEV_DEBUG(&spidev->dev, "irq: %d\n", spidev->irq);
+        DEV_DEBUG(&spidev->dev, "===============\n");
 }
 #define DEBUG_DUMP_SPI_DEVICE(dev) dump_spi_device(dev)
 #else
@@ -72,38 +71,38 @@ static void dump_crc32(struct device *dev) {
         u32 comp_crc = 0;
         u32 recv_crc = *( (u32*) &drvdata->rx_data[RX_BUFFER_SIZE - 4]);
 
-        dev_info(dev, "Received CRC: 0x%x\n", recv_crc);
+        DEV_DEBUG(dev, "Received CRC: 0x%x\n", recv_crc);
         comp_crc = ether_crc(RX_BUFFER_SIZE - sizeof(u32),
                         (unsigned char *) drvdata->rx_data);
-        dev_info(dev, "ether_crc:    CRC 0x%x, ICRC 0x%x\n",
+        DEV_DEBUG(dev, "ether_crc:    CRC 0x%x, ICRC 0x%x\n",
                         comp_crc, comp_crc ^ 0xffffffff);
 
         comp_crc = ether_crc_le(RX_BUFFER_SIZE - sizeof(u32),
                         (unsigned char *) drvdata->rx_data);
-        dev_info(dev, "ether_crc_le: CRC 0x%x, ICRC 0x%x\n",
+        DEV_DEBUG(dev, "ether_crc_le: CRC 0x%x, ICRC 0x%x\n",
                         comp_crc, comp_crc ^ 0xffffffff);
 
         comp_crc = crc32_le(~0, (unsigned char *) drvdata->rx_data,
                         RX_BUFFER_SIZE - sizeof(u32));
-        dev_info(dev, "crc_le:       CRC 0x%x, ICRC 0x%x\n",
+        DEV_DEBUG(dev, "crc_le:       CRC 0x%x, ICRC 0x%x\n",
                         comp_crc, comp_crc ^ 0xffffffff);
 
         comp_crc = crc32_be(~0, (unsigned char *) drvdata->rx_data,
                         RX_BUFFER_SIZE - sizeof(u32));
-        dev_info(dev, "crc_be:       CRC 0x%x, ICRC 0x%x\n",
+        DEV_DEBUG(dev, "crc_be:       CRC 0x%x, ICRC 0x%x\n",
                         comp_crc, comp_crc ^ 0xffffffff);
 
         comp_crc = crc32_le(0, (unsigned char *) drvdata->rx_data,
                         RX_BUFFER_SIZE - sizeof(u32));
-        dev_info(dev, "crc_le:       CRC 0x%x, ICRC 0x%x\n",
+        DEV_DEBUG(dev, "crc_le:       CRC 0x%x, ICRC 0x%x\n",
                         comp_crc, comp_crc ^ 0xffffffff);
 
         comp_crc = crc32_be(0, (unsigned char *) drvdata->rx_data,
                         RX_BUFFER_SIZE - sizeof(u32));
-        dev_info(dev, "crc_be:       CRC 0x%x, ICRC 0x%x\n",
+        DEV_DEBUG(dev, "crc_be:       CRC 0x%x, ICRC 0x%x\n",
                         comp_crc, comp_crc ^ 0xffffffff);
 
-        dev_info(dev, "[%s]\n", drvdata->rx_data);
+        DEV_DEBUG(dev, "[%s]\n", drvdata->rx_data);
 }
 #define DEBUG_DUMP_CRC32(dev) dump_crc32(dev)
 #else
@@ -125,7 +124,7 @@ static irqreturn_t bottom_ready_handler(int irq, void *dev_id) {
         /* Get state of READY GPIO pin */
         ready_pin = gpiod_get_value(drvdata->ready);
 
-        dev_info(&spidev->dev, "start spi");
+        DEV_DEBUG(&spidev->dev, "start spi");
 
         gpiod_set_value(drvdata->busy, 1);
         spi_ticks = ktime_get();
@@ -136,7 +135,7 @@ static irqreturn_t bottom_ready_handler(int irq, void *dev_id) {
                 dev_err(&spidev->dev, "get_block_sync failed %d\n", retval);
                 return IRQ_HANDLED;
         }
-        dev_info(&spidev->dev, "ended spi (ktime delta = %lld nsecs)",
+        DEV_DEBUG(&spidev->dev, "ended spi (ktime delta = %lld nsecs)",
                 spi_ticks);
 
         DEBUG_DUMP_CRC32(&spidev->dev);
@@ -149,7 +148,7 @@ static irqreturn_t bottom_ready_handler(int irq, void *dev_id) {
                         recv_crc, comp_crc);
                 return IRQ_HANDLED;
         }
-        dev_info(&spidev->dev, "READY state is %d, irq=%d\n", ready_pin, irq);
+        DEV_DEBUG(&spidev->dev, "READY state is %d, irq=%d\n", ready_pin, irq);
    
         return IRQ_HANDLED;
 }
@@ -177,11 +176,11 @@ static int get_block_sync(struct spi_device *spidev, size_t n, u8 *buf) {
 /**
  * @brief Probe SPI and GPIO
 */
-static int spi_probe(struct spi_device *spidev) {
+static int module_probe(struct spi_device *spidev) {
         int retval;
         drvdata_t *drvdata;
 
-        dev_info(&spidev->dev, "spi_probe\n");
+        DEV_DEBUG(&spidev->dev, "spi_probe\n");
 
         drvdata = (drvdata_t *) devm_kzalloc(&spidev->dev,
                                         sizeof(drvdata_t), GFP_KERNEL);
@@ -193,7 +192,7 @@ static int spi_probe(struct spi_device *spidev) {
                 dev_err(&spidev->dev, "Error in probe. Returned %d\n", retval);
                 return retval;
         }
-        dev_info(&spidev->dev, "spi_probe success\n");
+        DEV_DEBUG(&spidev->dev, "spi_probe success\n");
         drvdata->busy = devm_gpiod_get(&spidev->dev,
                                 "mycomp,busy", GPIOD_OUT_LOW);
         if (IS_ERR(drvdata->busy)) {
@@ -212,7 +211,7 @@ static int spi_probe(struct spi_device *spidev) {
                 dev_err(&spidev->dev, "gpiod_to_irq failed for READY pin\n");
                 return drvdata->irq;
         }
-        dev_info(&spidev->dev, "GPIOD irq = %d.\n", drvdata->irq);
+        DEV_DEBUG(&spidev->dev, "GPIOD irq = %d.\n", drvdata->irq);
 
         retval = request_threaded_irq(drvdata->irq,
                                  top_ready_handler,
@@ -225,12 +224,12 @@ static int spi_probe(struct spi_device *spidev) {
 
         DEBUG_DUMP_SPI_DEVICE(spidev);
 
-        dev_info(&spidev->dev, "GPIOD part probed succesfully.\n");
+        DEV_DEBUG(&spidev->dev, "GPIOD part probed succesfully.\n");
 
         return 0;
 };
 
-static int spi_remove(struct spi_device *spidev) {
+static int module_remove(struct spi_device *spidev) {
         drvdata_t *drvdata = spi_get_drvdata(spidev);
         if (!drvdata) {
                 dev_err(&spidev->dev, "Could not get driver data (remove).\n");
@@ -239,7 +238,7 @@ static int spi_remove(struct spi_device *spidev) {
         free_irq(drvdata->irq, spidev);
         gpiod_put(drvdata->ready);
         gpiod_put(drvdata->busy);
-        dev_info(&spidev->dev, "spi_remove\n");
+        DEV_DEBUG(&spidev->dev, "spi_remove\n");
         return 0;
 };
 
@@ -251,8 +250,8 @@ static const struct of_device_id spi_dt_ids[] = {
 };
 
 static struct spi_driver spi_driver = {
-        .probe = spi_probe,
-        .remove = spi_remove,
+        .probe = module_probe,
+        .remove = module_remove,
         .driver = {
                 .name = "spi-protocol-device",
                 .of_match_table = of_match_ptr(spi_dt_ids),
